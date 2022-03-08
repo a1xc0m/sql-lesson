64918c9c615591689f8f333ef22d97252b3e7c4e
a1xc0m <44187606+a1xc0m@users.noreply.github.com>
2022-03-08 14:57:30 +0300

    Update lesson_3_homework.sql

--- lesson_3_homework.sql
+++ lesson_3_homework.sql
@@ -94,10 +94,81 @@ from
 --task3
 --Корабли: Для классов, имеющих потери в виде потопленных кораблей и не менее 3 кораблей в базе данных, вывести имя класса и число потопленных кораблей.
 
+SELECT c.class, SUM(sh.sunked)
+FROM classes c
+  JOIN (
+     SELECT t.name AS name, t.class AS class,
+           CASE WHEN o.result = 'sunk' 
+           THEN 1 
+           ELSE 0 
+           END AS sunked
+     FROM
+     (
+      SELECT name, class
+      FROM ships
+       UNION
+      SELECT ship, ship
+      FROM outcomes
+     )
+     AS t
+    left JOIN outcomes o ON t.name = o.ship
+  ) sh ON sh.class = c.class
+GROUP BY c.class
+HAVING COUNT(DISTINCT sh.name) >= 3 AND SUM(sh.sunked) > 0
 
 --task4
 --Корабли: Найдите названия кораблей, имеющих наибольшее число орудий среди всех кораблей такого же водоизмещения (учесть корабли из таблицы Outcomes).
+select name
+from (select O.ship as name, numGuns, displacement
+from Outcomes O inner join
+Classes C on O.ship = C.class and
+O.ship not in (select name
+from Ships
+)
+union
+select S.name as name, numGuns, displacement
+from Ships S inner join
+Classes C on S.class = C.class
+) OS inner join
+(select max(numGuns) as MaxNumGuns, displacement
+from Outcomes O inner join
+Classes C on O.ship = C.class and
+O.ship not in (select name
+from Ships
+)
+group by displacement
+union
+select max(numGuns) as MaxNumGuns, displacement
+from Ships S inner join
+Classes C on S.class = C.class
+group by displacement
+) GD on OS.numGuns = GD.MaxNumGuns and
+OS.displacement = GD.displacement
 
 
 --task5
 --Компьютерная фирма: Найдите производителей принтеров, которые производят ПК с наименьшим объемом RAM и с самым быстрым процессором среди всех ПК, имеющих наименьший объем RAM. Вывести: Maker
+SELECT DISTINCT maker
+FROM product
+WHERE model IN (
+SELECT model
+FROM pc
+WHERE ram = (
+  SELECT MIN(ram)
+  FROM pc
+  )
+AND speed = (
+  SELECT MAX(speed)
+  FROM pc
+  WHERE ram = (
+   SELECT MIN(ram)
+   FROM pc
+   )
+  )
+)
+AND
+maker IN (
+SELECT maker
+FROM product
+WHERE type='printer'
+)
